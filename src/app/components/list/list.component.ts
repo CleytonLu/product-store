9f2372ad5ec7ed7153161074bba440bef7999883
cleyton.cabral <cleyton.cabral>
2024-09-19 17:17:50 -0300

    Criando um card para a lista de peodutos

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../services';
 import { Products } from '../../services/interfaces';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
