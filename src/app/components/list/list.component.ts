5fba28e548cd2541f4c50e1b7f1acca4fa799cab
CleytonLu <cleytonlucasiurd@gmail.com>
2024-10-24 14:43:35 -0300

    feat: Adiciona componente no-itens para a lista

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -4,11 +4,12 @@ import { Products } from '../../services/interfaces';
 import { CardComponent } from './components/card/card.component';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { NoItensComponent } from './components/no-itens/no-itens.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent, RouterLink, MatButtonModule],
+  imports: [CardComponent, RouterLink, MatButtonModule, NoItensComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
